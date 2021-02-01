ec160b1508cf4522e3b0ee6fce0042d302e8f0f2
Enrico Olivieri <enrico.olivieri@live.it>
2021-02-01 14:58:30 +0100

    table in

---
+++ database/migrations/2014_10_12_000000_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('guest_full_name');
+            $table->string('guest_credit_card')->unique();
+            $table->timestamp('room')->nullable();
+            $table->string('from');
+            $table->string('to');
+            $table->string('more_details');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
